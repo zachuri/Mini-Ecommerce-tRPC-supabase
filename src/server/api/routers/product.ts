cf6d9e2b436cc20f76dfe94438bcca085371a1a6
zachuri <punsalang.zachary@gmail.com>
2023-04-22 02:09:15 -0700

    trpc/product: added add products mutation

--- src/server/api/routers/product.ts
+++ src/server/api/routers/product.ts
@@ -1,13 +1,12 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { supabase } from "../../../utils/supabase-client";
 
 export const productRouter = createTRPCRouter({
   getProducts: publicProcedure
     // .input(z.object({ text: z.number() }))
     .query(async ({ ctx }) => {
       try {
-        const { data, error } = await supabase
+        const { data, error } = await ctx.supabaseClient
           .from("products")
           .select("*")
           .limit(10)
@@ -18,12 +17,38 @@ export const productRouter = createTRPCRouter({
         if (data) {
           return {
             allProducts: data,
-            session: ctx.session,
           };
         }
       } catch (error: any) {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         console.log(error.message);
+      }
+    }),
+  addProducts: publicProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        content: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const { data, error } = await ctx.supabaseClient
+          .from("user_posts")
+          .insert([
+            {
+              title: input.title,
+              content: input.content,
+              user_email: ctx.session?.user.email?.toLocaleLowerCase(),
+              user_id: ctx.session?.user.id,
+            },
+          ])
+          .single();
+        if (error) throw error;
+        return data;
+      } catch (error: any) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        console.log(error);
       }
     }),
 });
