7d3cc943ee0ef4c835ed2f461346fbb5e2d75759
zachuri <punsalang.zachary@gmail.com>
2023-04-22 22:18:59 -0700

    fetch-products: clean up/ look nicer

--- src/components/product/fetch-product.tsx
+++ src/components/product/fetch-product.tsx
@@ -6,20 +6,11 @@ const FetchProduct = () => {
   const getProducts = api.product.getProducts.useQuery();
 
   return (
-    <div className="flex flex-col gap-5 text-2xl">
+    <div className="mx-10 grid grid-cols-3 gap-5 text-2xl max-md:grid max-md:grid-cols-2">
       {getProducts.data?.allProducts?.map((product, index) => {
         return (
-          <div
-            key={index}
-            className="flex flex-row items-center justify-between border border-black"
-          >
-            <div>
-              <p>{product.name}</p>
-              <p>{product.description}</p>
-              <p>{product.price}</p>
-              <p>{product.category}</p>
-            </div>
-            <div className="relative h-[200px] w-[200px]">
+          <div key={index} className="flex flex-col ">
+            <div className="relative h-[150px] w-[150px] md:h-[200px] md:w-[200px]">
               <Image
                 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
                 src={product.image_url}
@@ -27,6 +18,12 @@ const FetchProduct = () => {
                 style={{ objectFit: "cover" }}
                 alt={"overlay"}
               />
+            </div>
+            <div className="my-5 flex flex-row items-start justify-between text-sm">
+              <p>{product.name}</p>
+              <p>${product.price}</p>
+              {/* <p>{product.description}</p> */}
+              {/* <p>{product.category}</p> */}
             </div>
           </div>
         );
